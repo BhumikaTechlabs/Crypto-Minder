95340518c1a0eb196ca41ee0e5d39ef4fd256328
Bhumika Saini <bs98.saini@gmail.com>
2018-07-02 18:44:33 +0530

    The code base

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Header, CryptoContainer } from './src/components';
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Provider store = { Store }>
+        <View>
+          <Header/>
+          <CryptoContainer/>
+        </View>
+      </Provider>
+    );
+  }
+
+}
